ba48b0221d031bd2f8b82aa2f19264a2a70d0987
EC2 Default User <ec2-user@ip-172-31-41-118.ec2.internal>
2021-09-07 09:20:59 +0000

    [Add]カートの商品削除・全商品削除追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,7 +1,8 @@
 class Public::CartItemsController < ApplicationController
 
  def index
-  @cart_items = CartItem.all
+  @cart_items = current_customer.cart_items
+  @total_price = 0
  end
 
  def create
@@ -20,9 +21,15 @@ class Public::CartItemsController < ApplicationController
  end
 
  def destroy
+  cart_item = CartItem.find(params[:id])
+  cart_item.destroy
+  redirect_to cart_items_path
  end
 
  def all_destroy
+  @cart_item = current_customer.cart_items
+  current_customer.cart_items.destroy_all
+  redirect_to cart_items_path
  end
 
  private
